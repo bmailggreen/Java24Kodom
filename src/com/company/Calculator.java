c59e8eba64817c6764becf0919bfdb8a776243fe
Gerjak Balazs <bmailggreen@gmail.com>
2021-11-06 08:45:50 +0100

    Feladat kiosztása.

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,5 +1,9 @@
 package com.company;
 
 public class Calculator {
-    
+    //Dorika90 összeadás függvény megírása
+
+    //kovma kivonás függvény megíráa
+
+    //Tami994 szorzás függvény megírása
 }
