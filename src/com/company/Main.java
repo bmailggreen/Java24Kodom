a2c6cedf2f2ab7e46d16907f399c0a57c3d750c6
Darvas Dóra <endor.ewok90@gmail.com>
2021-11-06 09:03:22 +0100

    osszeadas fuggveny

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Helló Világ!");
+
+        Calculator.osszead(3,4);
+
     }
+
+
+
 }
