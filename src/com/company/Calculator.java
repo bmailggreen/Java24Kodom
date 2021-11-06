88959413c509b031e7866838039ca6c09d651a9c
Gerjak Balazs <bmailggreen@gmail.com>
2021-11-06 09:07:31 +0100

    Szorzás hozzáadása.

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -3,12 +3,13 @@ package com.company;
 public class Calculator {
     //Dorika90 összeadás függvény megírása
 
-    //kovma kivonás függvény megíráa
+    //kovma kivonás függvény megírása
     public static int subtraction(int a, int b){
         return a - b;
     }
+
     //Tami994 szorzás függvény megírása
-
-
-
+    public static double multiplication(double a, double b) {
+        return a*b;
+    }
 }
