7157397df9113fcb83c72d13e822789d70e4c8a4
kovma <kovacsmatyas@ymail.com>
2021-11-06 08:56:54 +0100

    Kivonás statikus

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
     //Dorika90 összeadás függvény megírása
 
     //kovma kivonás függvény megíráa
-    public int subtraction(int a, int b){
+    public static int subtraction(int a, int b){
         return a - b;
     }
     //Tami994 szorzás függvény megírása
