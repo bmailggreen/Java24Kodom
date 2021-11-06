f61deef9732aaf52d726d9b0f50dabe43d4b1861
kovma <kovacsmatyas@ymail.com>
2021-11-06 08:50:00 +0100

    Kivonás

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -4,6 +4,11 @@ public class Calculator {
     //Dorika90 összeadás függvény megírása
 
     //kovma kivonás függvény megíráa
-
+    public int subtraction(int a, int b){
+        return a - b;
+    }
     //Tami994 szorzás függvény megírása
+
+
+
 }
